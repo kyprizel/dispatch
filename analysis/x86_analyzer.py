91e3a1b61b7a267b6a7abac450aa2cea6609a477
Nick Gregory <computerfreak97@gmail.com>
2016-03-07 18:29:36 -0500

    Copy paste coding is bad :(

--- analysis/x86_analyzer.py
+++ analysis/x86_analyzer.py
@@ -80,7 +80,7 @@ class X86_Analyzer(BaseAnalyzer):
                     if CS_GRP_CALL in ins.capstone_inst.groups:
                         call_addr = ins.capstone_inst.operands[-1].imm
                         if self.executable.vaddr_is_executable(call_addr):
-                            edge = CFGEdge(last_ins.address, last_ins.address + last_ins.size, CFGEdge.DEFAULT)
+                            edge = CFGEdge(ins.address, call_addr, CFGEdge.DEFAULT)
                             edges.add(edge)
 
                 for cur_bb in f.bbs:
@@ -89,15 +89,14 @@ class X86_Analyzer(BaseAnalyzer):
                     if CS_GRP_JUMP in last_ins.capstone_inst.groups:
                         if last_ins.capstone_inst.operands[-1].type == CS_OP_IMM:
                             jmp_addr = last_ins.capstone_inst.operands[-1].imm
-                            next_addr = last_ins.address + last_ins.size
 
                             if self.executable.vaddr_is_executable(jmp_addr):
-                                next_addr = last_ins.address + last_ins.size
-                                edge = CFGEdge(last_ins.address, next_addr, CFGEdge.COND_JUMP, True)
+                                edge = CFGEdge(last_ins.address, jmp_addr, CFGEdge.COND_JUMP, True)
                                 edges.add(edge)
                             if last_ins.mnemonic != 'jmp': # Looking for jne, jnz, etc.
                                 # Only add the "fall-through" case if the jump is conditional
                                 # TODO: opcode checking instead of mnemonic comparisons
+                                next_addr = last_ins.address + last_ins.size
                                 edge = CFGEdge(last_ins.address, next_addr, CFGEdge.COND_JUMP, False)
                                 edges.add(edge)
                     elif last_ins != f.instructions[-1]:
