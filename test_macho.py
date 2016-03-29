f91a52c3fec9f34f59a094b03eb7090ca35c0c68
Nick Gregory <computerfreak97@gmail.com>
2016-03-29 14:20:28 -0400

    Fixed MachO loading

---
+++ test_macho.py
@@ -0,0 +1,11 @@
+from formats import *
+
+import logging, struct, sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Load in the executable with read_executable (pass filename)
+executable = read_executable(sys.argv[1])
+
+# Invoke the analyzer to find functions
+executable.analyze()
